5e724b48afa16b222392edd937b25a13cfd4832b
hrushikesh-thorat <hrushikesh.thorat@sphererays.com>
2023-09-10 23:59:21 +0530

    Added folder structure + layout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="sl-nav_wrapper">
+    <div><img src="@/assets/logo.svg"/></div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.sl-nav_wrapper{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 50px;
+    background: #00056A;
+    box-shadow: 0px 2px 4px 0px rgba(141, 143, 169, 0.10);
+}
+</style>
---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <main>
+        <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+export default {
+    components:{
+        AppHeader
+}
+}
+</script>
+
+<style>
+
+</style>
